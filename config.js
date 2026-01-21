90e22459f088b4600deb6ab282c3115dea81c3bd
TitoMX <140332363+TitoYTMex23@users.noreply.github.com>
2026-01-21 15:34:29 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,42 @@
+const TITO_CONFIG = {
+    "color_principal": "#00e5ff",
+    "titulo_web": "TITOMX PRO",
+    "mensaje_bienvenida": "¿Qué vamos a ver hoy?",
+    "estilo_tarjetas": "normal",
+
+    // --- TIENDA ---
+    "tienda": [
+        { 
+            "nombre": "VIP 30 Días", 
+            "precio": "$1000 CUP", 
+            "imagen": "https://via.placeholder.com/300x200?text=VIP+Gold", 
+            "comando": "VIP 30 Dias" 
+        }
+    ],
+
+    // --- SERIES (NUEVO) ---
+    "series": [
+        { 
+            "titulo": "Breaking Bad", 
+            "imagen": "https://m.media-amazon.com/images/M/MV5BMMTU5MjgyOTA4NF5BMl5BanBnXkFtZTgwNzE3Mjg5MTE@._V1_FMjpg_UX1000_.jpg", // Foto vertical tipo poster
+            "info": "5 Temporadas | Full HD", 
+            "comando": "/serie breaking" 
+        },
+        { 
+            "titulo": "The Last of Us", 
+            "imagen": "https://m.media-amazon.com/images/M/MV5BZGUzYTI3M2EtZmM0Yy00NGUyLWI4ODEtN2Q3ZGJlYzhhZjU3XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg", 
+            "info": "Temp 1 | Latino", 
+            "comando": "/serie lastofus" 
+        }
+    ],
+
+    // --- NOVELAS (NUEVO) ---
+    "novelas": [
+        { 
+            "titulo": "Pasión de Gavilanes", 
+            "imagen": "https://es.web.img3.acsta.net/pictures/14/03/17/10/20/509671.jpg", 
+            "info": "Completa | 1080p", 
+            "comando": "/novela pasion" 
+        }
+    ]
+};
